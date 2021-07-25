b82ba066b81d864f76d177dd182e75bc5ca6cf33
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-25 16:53:11 +0200

    Add primary key demo

--- src/main/java/com/ky/hibernate/demo/PrimaryKeyDemo.java
+++ src/main/java/com/ky/hibernate/demo/PrimaryKeyDemo.java
@@ -1,13 +1,11 @@
 package com.ky.hibernate.demo;
 
+import com.ky.hibernate.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.ky.hibernate.demo.entity.Student;
-
-public class CreateStudentDemo {
-
+public class PrimaryKeyDemo {
     public static void main(String[] args) {
         // create session factory
         SessionFactory factory = new Configuration()
@@ -23,19 +21,39 @@ public class CreateStudentDemo {
 
             // create a student object
             System.out.println("Create Student Object");
-            Student student = new Student("Paul", "Wall", "paulwall@gmail.com");
+            Student student1 = new Student("John", "Doe", "john@gmail.com");
+            Student student2 = new Student("Mary", "Public", "mary@gmail.com");
+            Student student3 = new Student("Bonita", "Applebum", "bonita@gmail.com");
             // start a transaction
             session.beginTransaction();
             // save the student object
             System.out.println("Saving Student Object");
-            session.save(student);
+            session.save(student1);
+            session.save(student2);
+            session.save(student3);
             // commit transaction
             session.getTransaction().commit();
             System.out.println("Commit Successful");
+
+
+            // get new session and start transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // retrieve student based on the id: primary key
+            System.out.println("Getting student with id: " + student1.getId());
+
+            Student myStudent = session.get(Student.class, student1.getId());
+
+            System.out.println("Get complete: " + myStudent);
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
         }
         finally {
             factory.close();
         }
     }
-
 }
